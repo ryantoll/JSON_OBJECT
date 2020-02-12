7efe2a92ed9af9054cc958ec988c15e9da728b18
ryantoll <38963186+ryantoll@users.noreply.github.com>
2020-02-11 19:22:05 -0500

    Add files via upload

---
+++ __JSON_OBJECT.cpp
@@ -0,0 +1,125 @@
+#include "__JSON_OBJECT.h"
+using std::map;
+using std::string;
+using std::vector;
+
+// Returns a string representation of the JSON object
+string JSON_OBJECT::JSONObjectToString() {
+	string output{ "{" };																	// Begin with an open-curly-brace
+	if (singleValue.has_value()) { output = "\"" + *singleValue + "\""; return output; }	// Return single-value as string if it exists
+	else if (IsArray()) { return JSONArrayToString(*valueArray); }							// Print array if it is an array
+	else if (data.has_value()) {
+		for (auto prop : *data) {															// Print all properties if it's an object
+			output += "\"" + prop.first + "\":";
+			output += prop.second.JSONObjectToString();
+			output += ",";																	// Deliniate elements with commas
+		}
+		output.erase(output.size() - 1);													// Erase the trailing comma
+	}
+	else { return "null"; }																	// Print "null" to represent null value	
+
+	output += "}";																			// End with close-curly-brace
+	return output;
+}
+
+// Private helper function. Seperates out logic for building array strings.
+string JSON_OBJECT::JSONArrayToString(vector<JSON_OBJECT>& jsonArray) const {
+	string output{ "[" };																		// Open-square-bracket starts array
+	for (auto element : jsonArray) { output += element.JSONObjectToString(); output += ","; }	// Iterate over elements, add each one, and deliniate with commas
+	output.erase(output.size() - 1);															// Erase the trailing comma
+	output += "]";																				// Close-square-bracket ends array
+	return output;
+}
+
+// Builds current object to match JSON-encoded string
+JSON_OBJECT& JSON_OBJECT::StringToJSON(string& input) {
+	Reset();																							// Start by clearing out any old data
+	if (ClearEnclosingChars('[', ']', input)) { ParseJSONArrayString(input); return *this; }			// Build JSON array
+	else if (ClearEnclosingChars('"', '"', input)) { singleValue.emplace(input); return *this; }		// Return single-value JSON
+	else if (!ClearEnclosingChars('{', '}', input)) { singleValue.emplace(input);  return *this; }		// Erase {} on the two ends
+
+	// Build out JSON object by iterating over properties
+	size_t end{ 1 };
+	string key{ "" }, val{ "" };
+	JSON_OBJECT tempJSON;
+	while (end != string::npos && end < input.size()) {
+		end = input.find_first_of(":");
+		key = input.substr(0, end);
+		ClearEnclosingChars('"', '"', key);
+		input.erase(0, end + 1);							// Erase read portion as well as colon
+
+		end = input.find_first_of(",");
+		val = input.substr(0, end);
+		tempJSON.StringToJSON(val);
+		AddProperty(key, tempJSON);							// Recursively build
+		input.erase(0, end + 1);							// Erase read portion as well as comma
+	}
+	return *this;											// Return reference to self
+}
+
+// Private helper function. Separates logic for parsing arrays.
+void JSON_OBJECT::ParseJSONArrayString(string& input) {
+	string segment{ "" };
+	JSON_OBJECT tempJSON;
+	char delimiter{ '}' };															// Track if iterating through objects or single values
+
+	auto n = input.find_first_of(delimiter);										// Select based upon object first
+	if (n == string::npos) { delimiter = ','; input.find_first_of(delimiter); }		// Then try for single-values
+	while (input.size() > 0) {
+		segment = input.substr(0, n + 1);
+		tempJSON.StringToJSON(segment);
+		push_back(tempJSON);
+		n = input.find_first_of(delimiter);
+		input.erase(0, n + 2);
+	}
+}
+
+// Tests for enclosing chars and clears them out. Returns bool indicating success/failure.
+bool ClearEnclosingChars(const char c1, const char c2, string& s) {
+	if (s[0] != c1 || s[s.size() - 1] != c2) { return false; }
+	s.erase(0, 1);
+	s.erase(s.size() - 1);
+	return true;
+}
+
+// For arrays (and null objects)
+// Adds value to back of array. Turns null-object into an array with one element.
+// Throws if object is neither an array and nor null.
+JSON_OBJECT& JSON_OBJECT::push_back(const JSON_OBJECT& val) {
+	if (valueArray.has_value()) { valueArray->push_back(val); }
+	else {
+		if (!IsNull()) { std::logic_error e{ "Invalid JSON access [value array]. Only one non-null member may exist." }; throw e; }
+		vector<JSON_OBJECT> tempVec{ val };
+		valueArray.emplace(std::move(tempVec));
+	}
+	return *this;
+}
+
+// For object-type elements (and null objects)
+// Adds a new property to the element. Turns null-objects into object-type elements.
+// Throws if element is neither object-type nor null.
+JSON_OBJECT& JSON_OBJECT::AddProperty(const std::string& key, const JSON_OBJECT& val) {
+	if (data.has_value()) { data->emplace(key, val); }
+	else { 
+		if (!IsNull()) { std::logic_error e{ "Invalid JSON access [data map]. Only one non-null member may exist." }; throw e; }
+		map<string, JSON_OBJECT> tempMap{ {key, val} };
+		data.emplace(std::move(tempMap)); 
+	}
+	return *this;
+}
+
+// As above, except the element's value is the supplied string val
+JSON_OBJECT& JSON_OBJECT::AddProperty(const std::string& key, const std::string& val) { return AddProperty(key, JSON_OBJECT(val)); }
+
+// For object-type elements (and null objects)
+// Accesses property denoted by the supplied key.
+// Adds property with null JSON as value if property does not already exist (consistent with std::map).
+// Transforms null objects into object-type elements.
+// Throws if element is neither object-type nor null.
+JSON_OBJECT& JSON_OBJECT::operator[] (const std::string& key) {
+	try { return (*data)[key]; }
+	catch (std::bad_optional_access) {
+		if (!IsNull()) { std::logic_error e{ "Invalid JSON access [data map]. Only one non-null member may exist." }; throw e; }
+		AddProperty(key, JSON_OBJECT())[key];
+	}
+}
---
+++ __JSON_OBJECT.h
@@ -0,0 +1,73 @@
+/*///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Ryan Toll, 2020
+// This file lays out a provisional JSON library meant to create a bridge between the strong typing of C++ and the loose style of JSON.
+// Ideally, this is used only minimally. However, it may be very useful when interfacing with web-style tools that use JSON.
+// The objective is to create a reasonable approximation of a true JSON object within C++ with a decent user interface.
+//
+// JSON objects can contain: a single value, a single function, an array of JSON objects, or an object with 0+ properties, held as JSON objects.
+// JSON objects can be composed recursively indefinitely, so arrays and objects can branch out into any of the four types.
+// JSON objects must be dynamically composable at run-time.
+// JSON objects lack compile-time type safety. They therefore need run-time checks and additional exception handling.
+// 
+// This implementation uses std::optionals to store data given that the types are known and limited and object could contain any one type.
+// At any given time, at most one optional should be non-null. A completely null object is sensible and represents a null property.
+// All single values are stored as strings for universal access in C++. They can be converted as needed form there.
+// All functions take a JSON argument and return a JSON argument. Any function call should conceivably be convertable to this form.
+// Arrays are stored using std::vector and contain JSON elements, which can take arbitrary form.
+// Object-style elements are stored using std::map (enforcing default string sorting of properties). Properties are stored as JSON elements.
+// 
+// Getters are listed to give reference access to elements.
+// Inserts can be done with push_back or AddProperty as appropriate.
+// Overloaded operator[] also provides both string key and index access into internal map & vector respectively.
+// Single elements can be constructed explicitly using the appropriate constructor.
+// Unsupported operations can be done by directly accessing the map & vector references.
+*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __NETWORK_UTILITIES
+#define __NETWORK_UTILITIES
+
+#include <functional>
+#include <map>
+#include <memory>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+class JSON_OBJECT {
+public:
+	JSON_OBJECT() = default;
+	explicit JSON_OBJECT(std::string val) : singleValue(val) {};
+	explicit JSON_OBJECT(std::function<JSON_OBJECT(JSON_OBJECT)> func) : function(func) {}
+	
+	bool IsArray() const { return valueArray.has_value(); }
+	bool IsNull() const { return !singleValue.has_value() && !valueArray.has_value() && !data.has_value() && !function.has_value(); }
+	void Reset() { singleValue.reset(); valueArray.reset(); data.reset(); function.reset(); }			// Resets all data to null-values
+	std::string JSONObjectToString();
+	JSON_OBJECT& StringToJSON(std::string&);
+
+	JSON_OBJECT& push_back(const JSON_OBJECT&);
+	JSON_OBJECT& AddProperty(const std::string&, const std::string&);
+	JSON_OBJECT& AddProperty(const std::string&, const JSON_OBJECT&);
+	JSON_OBJECT& operator[] (size_t index) { return (*valueArray)[index]; }		// Access element of indexed array
+	JSON_OBJECT& operator[] (const std::string& key);
+	// Overload operator() for function calls?
+
+	// Getters. Each returns an optional that user is responsible for checking.
+	std::optional<std::string>& Val() { return singleValue; }
+	std::optional<std::function<JSON_OBJECT(JSON_OBJECT)>>& Func() { return function; }
+	std::optional<std::vector<JSON_OBJECT>>& Array() { return valueArray; }
+	std::optional<std::map<std::string, JSON_OBJECT>>& Map() { return data; }
+private:
+	std::optional<std::string> singleValue;								// Object may be a single value
+	std::optional<std::vector<JSON_OBJECT>> valueArray;					// ... or an array of values, which may in turn be objects themselves
+	std::optional<std::map<std::string, JSON_OBJECT>> data;				// ... or an object with multiple properties
+	std::optional<std::function<JSON_OBJECT(JSON_OBJECT)>> function;	// ... or a function object taking a JSON input and returning a JSON output
+	
+	std::string JSONArrayToString(std::vector<JSON_OBJECT>&) const;		// JSON to string utility function
+	void ParseJSONArrayString(std::string&);							// String to JSON utility function
+};
+
+bool ClearEnclosingChars(const char, const char, std::string&);
+
+#endif // !__NETWORK_UTILITIES
